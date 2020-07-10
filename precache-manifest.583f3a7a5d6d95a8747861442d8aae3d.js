ec99fae0708cb97292523e3c7aa50d194fc2fa36
Poisedboy <beschasnuy96@gmail.com>
2020-07-11 00:12:21 +0300

    Updates

---
+++ precache-manifest.583f3a7a5d6d95a8747861442d8aae3d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e0f409e0b18a6a39f850347110dcdd4",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "0def4c8d6ff8d8d71b2b",
+    "url": "/social-network/static/css/4.433e061e.chunk.css"
+  },
+  {
+    "revision": "30e32904385ac8ba33c9",
+    "url": "/social-network/static/css/5.28c08c43.chunk.css"
+  },
+  {
+    "revision": "267140773a10fef6d14e",
+    "url": "/social-network/static/css/6.883f2fa3.chunk.css"
+  },
+  {
+    "revision": "f3bfd132f16f60f07250",
+    "url": "/social-network/static/css/main.dfca22d7.chunk.css"
+  },
+  {
+    "revision": "f9e1078d862f7256c255",
+    "url": "/social-network/static/js/0.f3a90ac1.chunk.js"
+  },
+  {
+    "revision": "e794c11aea49e24283cb",
+    "url": "/social-network/static/js/3.33a11b24.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/social-network/static/js/3.33a11b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0def4c8d6ff8d8d71b2b",
+    "url": "/social-network/static/js/4.0b8a31f8.chunk.js"
+  },
+  {
+    "revision": "30e32904385ac8ba33c9",
+    "url": "/social-network/static/js/5.c91633d6.chunk.js"
+  },
+  {
+    "revision": "267140773a10fef6d14e",
+    "url": "/social-network/static/js/6.aec6da0d.chunk.js"
+  },
+  {
+    "revision": "f3bfd132f16f60f07250",
+    "url": "/social-network/static/js/main.6aea782a.chunk.js"
+  },
+  {
+    "revision": "bf9194594925ca8842b9",
+    "url": "/social-network/static/js/runtime-main.dd6f3a26.js"
+  },
+  {
+    "revision": "e67e358c72e30a5ec593534154b91ea6",
+    "url": "/social-network/static/media/1.e67e358c.png"
+  },
+  {
+    "revision": "8e683e9ec8df939845837f931eab453a",
+    "url": "/social-network/static/media/avatar.8e683e9e.jpg"
+  },
+  {
+    "revision": "e4b6323e19e57f6761b1ce47b7d329e0",
+    "url": "/social-network/static/media/preloader.e4b6323e.gif"
+  }
+]);
